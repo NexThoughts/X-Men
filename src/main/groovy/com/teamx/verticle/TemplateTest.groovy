77136f1279f63ae47521bee2f1d030cd2885dd30
abhilashj <abhilashjha@gmail.com>
2017-12-16 16:26:28 +0530

    Landing Page Created and client side validations added on front end

--- src/main/groovy/com/teamx/verticle/TemplateTest.groovy
+++ src/main/groovy/com/teamx/verticle/TemplateTest.groovy
@@ -3,7 +3,8 @@ package com.teamx.verticle
 import io.vertx.core.Vertx
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpHeaders;
-import io.vertx.ext.web.Router;
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
 class TemplateTest extends AbstractVerticle {
 
@@ -16,10 +17,9 @@ class TemplateTest extends AbstractVerticle {
         println "Landing Page"
 
         final Router router = Router.router(vertx);
-
         // In order to use a Thymeleaf template we first need to create an engine
         final ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
-
+        router.route("/static/*").handler(StaticHandler.create());
         // Entry point to the application, this will render a custom JADE template.
         router.get("/").handler({ ctx ->
 
@@ -33,8 +33,9 @@ class TemplateTest extends AbstractVerticle {
                 } else {
                     ctx.fail(res.cause());
                 }
-            });
-        });
+            })
+
+        })
 
         // start a HTTP web server on port 8080
         vertx.createHttpServer().requestHandler(router.&accept).listen(8087);
